e3f4c38f06988723b2a27295f834b6a5d833c0be
Ivan <ivansos7890@gmail.com>
2024-03-18 20:02:04 +0200

    18.03.2024

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,10 @@ Route::get('test', function (){
     app(\App\Services\Contract\FileStorageServiceInterface::class)->remove('test');
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', \App\Http\Controllers\HomeController::class)->name('home');
+
+Route::resource('products', \App\Http\Controllers\ProductsController::class)->only(['index', 'show']);
+Route::resource('categories', \App\Http\Controllers\CategoriesController::class)->only(['index', 'show']);
 
 Auth::routes();
 
@@ -27,4 +28,11 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::name('admin.')->prefix('admin')->middleware(['role admin|moderator'])->group(function (){
    Route::get('dashboard', \App\Http\Controllers\Admin\DashBoardController::class)->name('dashboard');//admin.dashboard
    Route::resource('categories', \App\Http\Controllers\Admin\CategoryController::class)->except(['show']);
+});
+
+Route::name('cart.')->prefix('cart')->group(function (){
+    Route::get('/', [\App\Http\Controllers\CartController::class, 'index'])->name('index');
+    Route::post('{product}', [\App\Http\Controllers\CartController::class, 'add'])->name('add');
+    Route::delete('/', [\App\Http\Controllers\CartController::class, 'remove'])->name('remove');
+    Route::post('{product}/count', [\App\Http\Controllers\CartController::class, 'countUpdate'])->name('count.update');
 });
